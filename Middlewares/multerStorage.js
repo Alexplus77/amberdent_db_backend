80412e380f520940f7649a22469909ab5c4f158e
Alex <alexplus77@mail.ru>
2024-08-27 20:57:54 +0200

    sdfvgbvc

--- Middlewares/multerStorage.js
+++ Middlewares/multerStorage.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Сохраняем файл csv в папку uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "..", "/uploads"));
+    cb(null, path.join("/uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
